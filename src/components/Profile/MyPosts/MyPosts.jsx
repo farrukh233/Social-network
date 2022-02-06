8dd4b2388678e671c810eb35c53e22b09454682e
farrukh <farrukhx50@gmail.com>
2022-02-07 03:03:09 +0500

    add props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,12 +11,8 @@ const MyPosts = () => {
         <button>Remove</button>
       </div>
       <div className={s.posts}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post message='Hi, Naruto' like='4' />
+        <Post message='Naruto-kun...' like='1' />
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,15 @@
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = props => {
   return (
     <div className={s.item}>
       <img
         src='http://static4.wikia.nocookie.net/__cb20131019150545/naruto/es/images/thumb/a/a0/Gaara_Capitan.png/300px-Gaara_Capitan.png'
         alt='gaara'
       />
-      post 1
+      {props.message}
       <div>
-        <span>Like</span>
+        <span>Like {props.like}</span>
       </div>
     </div>
   );
